1425b2c86d3b4435299c536907f54041759092d4
esSteres <elijahsteres@gmail.com>
2017-02-15 16:29:02 -0500

    Update PyGameProcessing.py

--- PyGameProcessing.py
+++ PyGameProcessing.py
@@ -3,18 +3,23 @@ import time
 #I know there will be other imports for data transmission, but those will be added when this is merged with roy's code
 
 pygame.init()
+pygame.event.setAllowed(pygame.JOYBUTTONDOWN)
 stick = pygame.joysick.Joystick(0)
 stick.init()
+lights = False
 try:
   while true:
-    pygame.event.pump()
+    events = pygame.event.get()
+    for event in events:
+      if event.button == 3:
+        lights = not lights
     fb = stick.get_axis(1)
     lr = stick.get_axis(0)
     motorL = int((-fb+lr)*250)
     motorR = int((-fb-lr)*250)
     motorV = stick.get_axis(4)*-250
-    print ({motorL, motorR, motorV})
-    time.sleep(0.1)
+    print ({motorL, motorR, motorV, lights})
+    time.sleep(0.09)
 except KeyboardInterrupt:
   stick.quit()
   pygame.quit()
