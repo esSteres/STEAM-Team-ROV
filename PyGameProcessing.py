d759a1bca237332e103b43ec2287271a55677838
esSteres <elijahsteres@gmail.com>
2017-02-15 15:57:54 -0500

    Update PyGameProcessing.py

--- PyGameProcessing.py
+++ PyGameProcessing.py
@@ -1,19 +1,20 @@
 import pygame
+import time
 #I know there will be other imports for data transmission, but those will be added when this is merged with roy's code
 
 pygame.init()
-pygame.event.set_allowed(pygame.JOYAXISMOTION)
 stick = pygame.joysick.Joystick(0)
 stick.init()
 try:
   while true:
-    event = pygame.event.wait()
+    pygame.event.pump()
     fb = stick.get_axis(1)
     lr = stick.get_axis(0)
     motorL = int((-fb+lr)*250)
     motorR = int((-fb-lr)*250)
     motorV = stick.get_axis(4)*250
     print ({motorL, motorR, motorV})
+    time.sleep(0.1)
 except KeyboardInterrupt:
   stick.quit()
   pygame.quit()
